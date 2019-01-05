ca61e704cf89c8cde391d92d6a5b4ab1aa61200a
santmato <santiago.mato.rpc@gmail.com>
2019-01-04 23:35:18 -0300

    added without heroku version

--- server.js
+++ server.js
@@ -44,3 +44,57 @@ app.post('/imageUrl', (req, res) => {image.apiCall(req, res)})
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running in port ${process.env.PORT}`);
 });
+
+/* 
+//File without Heroku config 
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex({
+    client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '1234',
+    database : 'smart-brain'
+    }
+  });
+
+db.select('*').from('users').then( data => {
+    console.log(data);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('it is working');
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleGetProfile(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.post('/imageUrl', (req, res) => {image.apiCall(req, res)})
+
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
+
+*/
